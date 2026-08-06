b7ccaacf4a9149592463dc8c26e84a94db91c64d
philipmayoradebayo-collab <philipmayoradebayo@gmail.com>
2026-08-06 09:12:06 -0700

    Use environment variable for SECRET_KEY
    Replaced hardcoded SECRET_KEY with environment variable.

--- agroproject/settings.py
+++ agroproject/settings.py
@@ -25,7 +25,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8*nypm5shnth)2k96zoyp6ldezdzs+qr@1+ifqkjnlxi18drv="
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 from dotenv import load_dotenv
 import os
